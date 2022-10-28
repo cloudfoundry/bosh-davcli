ce13b452beb1846ab3c3a00b39e0abd00f3e4d9e
Rajan Agaskar <ragaskar@vmware.com>
2022-10-27 17:26:56 -0700

    Ensure promoted builds use same Go version as tests.
    - This pipeline contained a race condition: an binary could be built
      against a Golang version that was not used for tests. This is because
      the container providing the golang toolchain was not subject to pass
      constraints. Admittedly there is a low likelihood of encountering this
      scenario as the tests are relatively fast and a new bosh-s3 task image
      (which provides the go toolchain for tests and build jobs) would have
      to be created in between test start and final build job. However, this
      change ensures this will not be an issue.
    - Also: bump when a new patch is added to golang-release. Previously
      this build may not have run when a new patch was added to golang
      release (because it built off of source changes made by the bump-deps
      build). Now it will build whenever a s3cli-docker-image change
      occurs, which will happen with every golang-release change. This
      should keep the binary built against the latest go toolchain checked
      into golang-release.

--- ci/configure.sh
+++ ci/configure.sh
@@ -8,4 +8,5 @@ fi
 
 fly -t bosh-ecosystem set-pipeline -p bosh-davcli -c ci/pipeline.yml \
   -l <(lpass show -G "davcli concourse secrets" --notes) \
-  -l <(lpass show --notes "pivotal-tracker-resource-keys")
+  -l <(lpass show --notes "pivotal-tracker-resource-keys") \
+  -l <(lpass show --note "bosh:docker-images concourse secrets")
