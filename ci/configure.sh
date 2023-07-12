fe65385e1f11b8661ecdf14f7a9c25e4b12131e9
Joseph Palermo <jpalermo@pivotal.io>
2023-07-12 20:25:40 +0800

    Update repo url to cloudfoundry org

--- ci/configure.sh
+++ ci/configure.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if [[ $(lpass status -q; echo $?) != 0 ]]; then
-  echo "Login with lpass first"
-  exit 1
-fi
-
-fly -t bosh-ecosystem set-pipeline -p bosh-davcli -c ci/pipeline.yml \
-  -l <(lpass show -G "davcli concourse secrets" --notes)
+fly -t bosh-ecosystem set-pipeline -p bosh-davcli -c ci/pipeline.yml
