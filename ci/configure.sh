222af33a70fd63d8078c13ad2fedd3688a7a167b
Joseph Palermo <jpalermo@pivotal.io>
2023-07-27 09:51:46 +0800

    Move artifacts bucket and pipeline to bosh concourse instance

--- ci/configure.sh
+++ ci/configure.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-fly -t bosh-ecosystem set-pipeline -p bosh-davcli -c ci/pipeline.yml
+fly -t bosh set-pipeline -p bosh-dav-cli -c "$(dirname "${0}")/pipeline.yml"
